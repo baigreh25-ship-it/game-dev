d11e0f767c8bc81b6dea581e069704c5b44519c8
baigreh25-ship-it <baigreh25@gmail.com>
2026-01-19 17:55:32 +0000

    Add files via upload

--- lion and antalope.py
+++ lion and antalope.py
@@ -5,6 +5,7 @@ WIDTH=500
 HEIGHT=400
 
 score=0
+gameover=False
 lion=Actor("lion.png")
 lion.pos=250,200
 antalope=Actor("antalope.png")
@@ -12,13 +13,19 @@ antalope.pos=150,120
 
 
 def draw():
-    screen.fill("light grey")
+    screen.blit("bgforest.jpg",(0,0))
     lion.draw()
     antalope.draw()
     screen.draw.text("CATCH THE ANTALOPE!!!",(200,50))
     screen.draw.text(f"score:{score}",(40,120))
+    if gameover:
+        screen.fill("Red")
+        screen.draw.text("GAMEOVER",(250,250))
+        screen.draw.text(f"Your final score is {score}",(300,200))
+
 
 def update():
+    global score
     if keyboard.up:
         lion.y-=2
     if keyboard.down:
@@ -27,5 +34,17 @@ def update():
         lion.x-=2
     if keyboard.right:
         lion.x+=2
-    
+    if lion.colliderect(antalope):
+        antalope.x=random.randint(50,WIDTH-50)
+        antalope.y=random.randint(50,HEIGHT-50)
+        sounds.eep.play()
+        score+=1
+
+def endgame():
+    global gameover
+    gameover=True
+
+clock.schedule(endgame,20)
+
+
 pgzrun.go()
